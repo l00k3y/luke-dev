017ff7b73fb5d98af3e7a8a0fc963c459212fe8d
Luke <lukebretherton@outlook.com>
2025-04-19 11:41:02 +0100

    Fixing appearance on mobile

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 
 import "./footer.css";
 
@@ -10,7 +10,13 @@ const Footer = () => {
           © 2025 Luke Chisnall.dev
         </Typography>
         <Typography variant="body2" textAlign={"right"}>
-          Built with React and TypeScript | Deployed on Pages | Source on GitHub
+          Built with React and TypeScript | Deployed on Vercel |{" "}
+          <Link
+            href="https://github.com/l00k3y/luke-dev"
+            target="_blank"
+            rel="noopener noreferrer">
+            Source on GitHub
+          </Link>
         </Typography>
       </div>
     </footer>
