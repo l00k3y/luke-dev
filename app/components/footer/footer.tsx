1c470a0c19460cbeffe95d54b1cd0dc78d741193
Luke <lukebretherton@outlook.com>
2025-04-26 08:28:18 +0100

    Updating footer responsiveness

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { Link, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 import "./footer.css";
 
@@ -6,18 +6,24 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-text-container">
-        <Typography variant="body2" textAlign={"left"}>
-          © 2025 Luke Chisnall.dev
-        </Typography>
-        <Typography variant="body2" textAlign={"right"}>
-          Built with React and TypeScript | Deployed on Vercel |{" "}
-          <Link
-            href="https://github.com/l00k3y/luke-dev"
-            target="_blank"
-            rel="noopener noreferrer">
-            Source on GitHub
-          </Link>
-        </Typography>
+        <Typography variant="body2">© 2025 Luke Chisnall.dev</Typography>
+
+        <Box id="footer-right-text" textAlign={"center"}>
+          <Typography variant="body2">
+            Built with React and TypeScript
+          </Typography>
+          <Typography ml={1} variant="body2">
+            <Link
+              href="https://github.com/l00k3y/luke-dev"
+              target="_blank"
+              rel="noopener noreferrer">
+              Source on GitHub
+            </Link>
+          </Typography>
+          <Typography ml={1} variant="body2">
+            Deployed on Vercel
+          </Typography>
+        </Box>
       </div>
     </footer>
   );
